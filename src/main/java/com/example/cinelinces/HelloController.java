27b9324dbe7649794432cbdcf5a6e4ebe263c58a
juanp <jpabloom13@gmail.com>
2025-05-17 01:36:17 -0600

    refactor UI layout and add styles for sidebar and main content

--- src/main/java/com/example/cinelinces/HelloController.java
+++ src/main/java/com/example/cinelinces/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.cinelinces;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
@@ -16,5 +17,23 @@ public class HelloController {
     @FXML
     protected void onAnimatedButtonClick() {
         welcomeText.setText("¡El botón animado funciona!");
+    }
+
+    public void onEstrenos(ActionEvent actionEvent) {
+    }
+
+    public void onProximamente(ActionEvent actionEvent) {
+    }
+
+    public void onPopulares(ActionEvent actionEvent) {
+    }
+
+    public void onMiCuenta(ActionEvent actionEvent) {
+    }
+
+    public void onSearch(ActionEvent actionEvent) {
+    }
+
+    public void onVerHorarios(ActionEvent actionEvent) {
     }
 }
