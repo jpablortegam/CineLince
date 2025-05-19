049480abd9dfa9347bd6d6963201fe5fb5462214
juanp <jpabloom13@gmail.com>
2025-05-18 20:30:25 -0600

    refactor UI layout and enhance styles for main application view

--- src/main/java/com/example/cinelinces/HelloApplication.java
+++ src/main/java/com/example/cinelinces/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 640);
         scene.getRoot().setStyle("-fx-enable-animations: true;");
         stage.setTitle("Hello!");
         stage.setScene(scene);
